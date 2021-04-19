7a3d7f751046a35ade5e8734bdb4d615f858cdca
unknown <renata.cavalcanti.garcia@gmail.com>
2021-04-19 10:26:47 -0300

    cria branch Renata

---
+++ config/config.js
@@ -0,0 +1,10 @@
+{
+  module.exports = {
+    "username": "root",
+    "password": "1234",
+    "database": "virapagina",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "port":"3306"
+   }
+}
